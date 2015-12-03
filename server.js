9b233571abeadcda835c52917302f3039758c7b3
anonym0us3 <nathan.shayevich@gmail.com>
2015-12-03 15:21:11 -0800

    changes bein' made!

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -39,11 +39,25 @@ app.get('/', function homepage (req, res) {
 
 app.get('/api/todos/search', function search(req, res){});
 
-app.get('/api/todos', function index(req, res) {});
+app.get('/api/todos', function index(req, res) {
+	res.json({todos: todos});
+});
 
 app.post('/api/todos', function create(req, res) {});
 
-app.get('/api/todos/:id', function show(req, res) {});
+// app.get('/api/todos/:id', function show(req, res) {
+// 	res.json(todos[req.params._id]);
+// });
+
+app.get('/api/todos/:id', function show (req, res) {
+  var todoId = parseInt(req.params.id);
+  console.log(req.params);
+  // How woud you grab the todo with that id?
+  todos.filter( function ( element ) {
+	// if the element's _id is equal to todoID, then return it.
+  });
+  console.log(todoID + 1);
+});
 
 app.put('/api/todos/:id', function update(req, res) {});
 
