53eccb33d601d351ee21df9a84bb2a64590ac2a5
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2015-12-03 14:15:56 -0800

    CRUD

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -39,15 +39,40 @@ app.get('/', function homepage (req, res) {
 
 app.get('/api/todos/search', function search(req, res){});
 
-app.get('/api/todos', function index(req, res) {});
+app.get('/api/todos', function index(req, res) {
+	res.json({todos: todos});
+});
 
-app.post('/api/todos', function create(req, res) {});
+app.post('/api/todos', function create(req, res) {
+	var newTodo = req.body;
+	if (todos.length > 0) {
+		newTodo._id = todos[todos.length - 1]._id + 1;
+	} else {
+		newTodo._id = 1;
+	}
+	todos.push(newTodo);
+	res.json(newTodo);
+});
 
-app.get('/api/todos/:id', function show(req, res) {});
+app.get('/api/todos/:id', function show(req, res) {
+	var idx = req.params.id - 1;
+	res.send(todos[idx]);
+});
 
-app.put('/api/todos/:id', function update(req, res) {});
+app.put('/api/todos/:id', function update(req, res) {
+	var updatedTask = req.body.task;
+	var updatedDescription = req.body.description;
+	var idx = req.params.id - 1;
+	todos[idx].task = updatedTask;
+	todos[idx].description = updatedDescription;
+	res.json(todos[idx]);
+});
 
-app.delete('/api/todos/:id', function destroy(req, res) {});
+app.delete('/api/todos/:id', function destroy(req, res) {
+	var idx = req.params.id - 1;
+	todos.splice(idx, 1);
+	res.json(todos);
+});
 
 
 /**********
