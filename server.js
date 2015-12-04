76953df7bea9f39c3d1c58800f4eec44860f562a
Daniel Lwo <danielxji6@gmail.com>
2015-12-04 10:40:26 -0800

    w3d4 homework

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -32,22 +32,72 @@ app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/search', function searchpage (req, res) {
+  res.sendFile(__dirname + '/views/search.html');
+});
+
 
 /*
  * JSON API Endpoints
  */
 
-app.get('/api/todos/search', function search(req, res){});
+app.get('/api/todos/search', function search(req, res){
+  var data = {todos: []};
+  todos.forEach(function (ele, index) {
+    if(ele.task == req.query.q) {
+      data.todos.push(todos[index]);
+      res.json(data);
+    }
+  });
+  res.send({todos: [{task:"No data!"}]});
+});
 
-app.get('/api/todos', function index(req, res) {});
+app.get('/api/todos', function index(req, res) {
+  var data = {todos: todos};
+  res.json(data);
+});
 
-app.post('/api/todos', function create(req, res) {});
+app.get('/api/todos/:id', function show(req, res) {
+  var data;
+  todos.forEach(function (ele, index) {
+    if(ele._id == req.params.id) {
+      data = todos[index];
+    }
+  });
+  res.json(data);
+});
 
-app.get('/api/todos/:id', function show(req, res) {});
+app.post('/api/todos', function create(req, res) {
+  var data = req.body;
+  data._id = 0;
+  todos.forEach(function (ele, index) {
+    data._id = ele._id >= data._id ? ele._id+1 : data._id;
+  });
+  todos.push(data);
+  res.json(data);
+});
 
-app.put('/api/todos/:id', function update(req, res) {});
+app.put('/api/todos/:id', function update(req, res) {
+  var data = req.body;
+  todos.forEach(function (ele, index) {
+    if(ele.task == data.task) {
+      todos[index].task = data.task;
+      todos[index].description = data.description;
+      res.json(todos[index]);
+    }
+  });
+});
 
-app.delete('/api/todos/:id', function destroy(req, res) {});
+app.delete('/api/todos/:id', function destroy(req, res) {
+  var data;
+  todos.find(function (ele, index) {
+    if(ele._id == req.params.id) {
+      data = todos[index];
+      todos.splice(index, 1);
+      res.json(data);
+    }
+  });
+});
 
 
 /**********
