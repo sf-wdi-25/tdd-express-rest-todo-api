1a8abdb6ea7ab2b256776b3f04ddfb6f319bd5ca
isangieri <isabelleangieri@gmail.com>
2015-12-03 17:07:02 -0800

    created search.html and changed the input labels

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-	res.sendFile('views/index.html', {root : __dirname});
+app.get('/api/todos/search', function (req, res) {
+	res.sendFile('views/search.html', {root : __dirname});
 });
 
 /************
